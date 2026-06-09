65e5c2fab5452e68dff1d7121e89a3256388c1c7
merlin <allesman.stachelx@gmail.com>
2026-06-09 19:44:26 +0200

    sync

---
+++ ImageProcessing/util/test_grid_unshifted.py
@@ -0,0 +1,19 @@
+from src.TypewriterDriver import RpiTypeWriter
+from src.type.ASCII import ASCII
+
+# 3x4 grid using only unshifted (non-modifier) chars
+grid = [
+    [' ', '8', '4', '3'],
+    [';', '2', '=', ' '],
+    [',', '3', '8', ';'],
+]
+
+ascii_art = ASCII(grid=grid, width=4, height=3, charset="")
+
+driver = RpiTypeWriter()
+try:
+    driver.setup()
+    driver.print_ascii(ascii_art)
+finally:
+    driver.cleanup()
+
