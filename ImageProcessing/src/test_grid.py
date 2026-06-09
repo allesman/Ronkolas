fdca05d5947b32005085663a5e6dac83cec333ce
merlin <allesman.stachelx@gmail.com>
2026-06-09 19:43:23 +0200

    sync

---
+++ ImageProcessing/src/test_grid.py
@@ -0,0 +1,19 @@
+from TypewriterDriver import RpiTypeWriter
+from type.ASCII import ASCII
+
+# 3x4 grid using only mapped chars
+grid = [
+    [' ', '8', '*', '#'],
+    [';', ':', '4', ' '],
+    ['3', '=', '+', '2'],
+]
+
+ascii_art = ASCII(grid=grid, width=4, height=3, charset="")
+
+driver = RpiTypeWriter()
+try:
+    driver.setup()
+    driver.print_ascii(ascii_art)
+finally:
+    driver.cleanup()
+
