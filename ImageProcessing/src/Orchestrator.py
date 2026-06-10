7c06377cf01fd1496b7cc09cc904cc8b4e629a0d
CharlotteUn <charlotte.unsoeld@web.de>
2026-06-10 11:55:43 +0200

    button_listener und modi Variable im Orchestrator

--- ImageProcessing/src/Orchestrator.py
+++ ImageProcessing/src/Orchestrator.py
@@ -13,15 +13,18 @@ from type.ASCII import ASCII
 USB_Path = Path("/media/pi")       #unsure was hier der richtige Path ist, nachschauen auf raspi, wie der usb stick abspeichert? evtl /media/pi TODO:
 #potentially needs adjustment in img loading as unsure how usb stick works
 CONTRAST_FAC = 1.0
+MODE_STANDARD = "standard"
+MODE_ARTISTIC = "artistic"
 
 class Orchestrator:
-    def __init__(self, contrast_factor: float) -> None:
+    def __init__(self, contrast_factor: float, mode: str = MODE_STANDARD) -> None:
         self._loader = BmpImageLoader()
         self._preProc = ImagePreprocessor()
         self._converter = RampAsciiConverter()
         self._driver = RpiTypeWriter()
         self._log = StdLogger()
         self._contrast = contrast_factor
+        self._mode = mode
         
     def run(self) -> bool:
         try:
@@ -66,7 +69,14 @@ class Orchestrator:
         normalized = self._preProc.normalize(gray)
         contrasted = self._preProc.adjust_contrast(normalized, self._contrast)
         self._log.info("preprocessing done")
-        ascii = self._converter.convert(contrasted)
+
+        if self._mode == MODE_ARTISTIC:
+            self._log.info("artistic mode")
+            #TODO: implement Artisitc Variation
+            ascii = self._converter.convert(contrasted)
+        else:
+            ascii = self._converter.convert(contrasted)
+
         self._log.info("conversion done")
         return ascii
     
@@ -94,7 +104,7 @@ if __name__ == "__main__":
         format="%(asctime)s [%(levelname)s] %(message)s"
     )
     
-    orchestrator = Orchestrator(contrast_factor = CONTRAST_FAC)
+    orchestrator = Orchestrator(contrast_factor = CONTRAST_FAC, mode=MODE_STANDARD)
     success = orchestrator.run()
     
     if not success:
