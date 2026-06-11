ef50d6b0855f819b170217c4e12199bebeaab743
merlin <allesman.stachelx@gmail.com>
2026-06-11 10:54:50 +0200

    add test relevant things in other relevant files

--- ImageProcessing/util/pi-setup.bash
+++ ImageProcessing/util/pi-setup.bash
@@ -5,5 +5,9 @@ python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 
+#OPTIONAL: run the tests to make sure everything is working
+pytest tests
+# (Run the files you wanna run)
+
 # IMPORTANT: BEFORE UNPLUGGING:
 sudo shutdown -h now
