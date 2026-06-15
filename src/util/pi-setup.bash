60e4e75e3aafeabee6f91ea5198ded1eec09ed1d
Merlin Rehbinder <67746449+allesman@users.noreply.github.com>
2026-06-15 19:09:28 +0200

    Update pi-setup.bash

--- src/util/pi-setup.bash
+++ src/util/pi-setup.bash
@@ -1,8 +1,8 @@
 #sudo apt-get install bash-completion # lowk unnecessary
 git clone https://github.com/allesman/Ronkolas.git
 cd Ronkolas
-python3 -m venv venv
-source venv/bin/activate
+python3 -m venv .venv
+source .venv/bin/activate
 pip install -r requirements.txt
 
 #OPTIONAL: run the tests to make sure everything is working
